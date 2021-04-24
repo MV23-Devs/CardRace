1b917266a359b35fe438c635a4402f30542a324b
Atli Arnarsson <atli.nikulas@gmail.com>
2021-04-24 09:04:45 -0700

    working on adding homepage/joinpage

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="top">
+    <div id="titleArea">
+      <h1>
+        CardRace
+        <!-- should be logo image soon -->
+      </h1>
+    </div>
+    <div id="navbar">
+      <button class="navbarBtn">About</button>
+    </div>
+  </div>
+</template>
+
+
+<style src="../assets/css/Navbar.css">
+</style>
