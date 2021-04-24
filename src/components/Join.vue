3dc0e9d854dca32f97d767be30a3beb2635a4282
Atli Arnarsson <atli.nikulas@gmail.com>
2021-04-24 10:47:07 -0700

    pain

--- src/components/Join.vue
+++ src/components/Join.vue
@@ -1,11 +1,41 @@
+
+
 <template>
   <div id="main">
-    <button id="joinMeeting">Join a meeting</button>
-    <div id="popup">
+    <!-- <button id="joinMeeting" @click="!isHidden">Join a meeting</button> -->
+    <div id="popup" >
       <input type="text" id="input-code" placeholder="Meeting ID" />
       <button id="joinBtn">Join</button>
     </div>
   </div>
 </template>
+
+
+
+// <script>
+// var app = new Vue({
+//   el: '#main',
+//   data: {
+//     isHidden: false
+//   },
+//   methods: {
+//     getHidden() {
+//       return isHidden
+//     }
+//   }
+// })
+
+// export default {
+//   name: "Join",
+  
+//   data() {
+//     return {
+//         app,getHidden()
+//     }
+//   }
+// };
+// </script>
+
+
 <style scoped src="../assets/css/Join.css">
 </style>
