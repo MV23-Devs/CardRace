5366478ec43fc2dd8789ad9a09c21db3eccf34c4
Jacob Ismael <jacob.ismael@gmail.com>
2021-04-24 10:06:05 -0700

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="app">
-    <Navbar />
-    <Join/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Navbar from "./components/Navbar.vue";
-import Join from "./components/Join.vue";
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: "App",
-  components: {
-    Navbar,
-    Join,
-  },
-};
+#nav {
+  padding: 30px;
+}
 
-</script>
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-<style src="./assets/css/App.css">
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
