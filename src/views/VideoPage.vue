f7d8978bac9ae31b6cf91d9b8cc32b99fa20bedc
saarangbond <48871052+saarangbond@users.noreply.github.com>
2021-04-24 21:59:07 -0700

    merging tactics

--- src/views/VideoPage.vue
+++ src/views/VideoPage.vue
@@ -1,108 +1,116 @@
-
 <template>
-  <div id="VideoPage">
-    <VideoChat id="vidChat" />
-
-    
-
-    <div id="notVideo">
-      <!-- mute -->
-      <button id="muteButton" class="iconButtons" v-if="micOn" v-on:click="mute"
-        ><img src="../assets/microphone.png" width="35" height="35"
-      /></button>
-      <button id="muteButton" class="iconButtons" v-else v-on:click="mute"
-        ><img src="../assets/microphone-off.png" width="35" height="35"
-      /></button>
-      <!-- video  -->
-      <button
-        id="cameraButton"
-        class="iconButtons"
-        v-if="camOn"
-        v-on:click="cameraFlip"
-        ><img src="../assets/camera.png" width="35" height="35"
-      /></button>
-      <button
-        id="cameraButton"
-        class="iconButtons"
-        v-else
-        v-on:click="cameraFlip"
-        ><img src="../assets/camera-off.png" width="35" height="35"
-      /></button>
+  <div id="playArea">
+    <div id="left">
+      <div id="FlashcardHint"></div>
+      <form id="form" v-on:submit.prevent="answerSubmitHandler">
+        <!-- <label> Check Answers </label> -->
+        <input
+          type="text"
+          placeholder="Answer Here"
+          id="answerInput"
+          v-model="answerInput"
+        />
+        <input type="submit" id="submit" />
+      </form>
+    </div>
+    <div id="right">
+      <div id="timer">
+        <p>Leaderboard</p>
+        <!-- <button v-on:click="sortScoreboard">Sort Scoreboard</button> -->
+        <ol>
+          <li v-for="item in sortScoreboard(scores)" v-bind:key="(item.name, item.score)">
+            <p>{{item.name}}: {{item.points}}</p>
+          </li>
+        </ol>
+      </div>
       
+      <div id="log">
+        <p>Guesses</p>
+        <ul>
+          <li v-for="item in guesses" v-bind:key="item">
+            <p>{{item}}</p>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
-
 <script>
-import VideoChat from "../components/VideoChat.vue";
-
+import { firebase } from "@firebase/app";
+import "firebase/auth";
 
 export default {
-  name: "App",
-  components: {
-    VideoChat,
+  name: "Play",
+  props: {
+    answers: Array,
   },
-
-  mounted() {
-    // this.load();
-
-    // if (firebase.auth().currentUser) {
-    //   firebase
-    //     .firestore()
-    //     .collection("accounts")
-    //     .doc(firebase.auth().currentUser.email)
-    //     .collection("collections")
-    //     .doc("Test")
-    //     .collection("cards")
-    //     .doc("test2")
-    //     .get()
-    //     .then((doc) => {
-    //       console.log(doc.data());
-    //       this.answer = doc.data().val;
-    //       console.log(this.answer);
-    //     });
-    // }
-  },
-
   data() {
     return {
-      collections: [],
-      cards: [],
-      key: "",
-      val: "",
-      name: "",
-      current: "",
-      ccurent: "",
-      open: false,
-      userInputs: [],
-      answer: "",
-      user: "",
-      userInput: "",
-      userPoints: 0,
-      opponentPoints: 0,
-      micOn: true,
-      camOn: true,
-      players: []
+      answerInput: "",
+      scores: [],
+      guesses: [],
     };
   },
+  created() {
+    firebase.firestore().collection("meetings").doc("please").collection("users").get().then((querySnapshot) => {
+      let storeScores = [];
+      querySnapshot.forEach((doc) => {
+        storeScores.push(doc.data());
+      })
+      console.log(storeScores);
+      this.scores = storeScores;
+    })
+  },
   methods: {
-    mute() {
-      console.log("Mute was called");
-      // if (micOn) {
-      //   localStreams.camera.stream.muteAudio();
-      // } else {
-      //   localStreams.camera.stream.unmuteAudio();
-      // }
-      this.micOn = !this.micOn;
+    answerSubmitHandler() {
+      let answers = [];
+      let flashcardKey = "jacob";
+      let collectionName = "Countries"
+
+      firebase.firestore().collection("collections").doc(collectionName).collection("cards").get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          answers.push(doc.data());
+        })
+      })
+      console.log("answers", answers);
+      this.guesses.push(this.answerInput);
+      
+      //console.log("lesgo");
+      let correct = this.checkAnswer(this.answerInput, flashcardKey, answers);
+      if(correct){
+        this.guesses.push("GUESS CORRECT!");
+      }
     },
-    cameraFlip() {
-      console.log("cameraFlip was called");
-      this.camOn = !this.camOn;
+    checkAnswer(userInput, key, answers) {
+      for (let i = 0; i < answers.length; i++) {
+        if (answers[i].key == key) {
+          if (userInput == answers[i].val) {
+            //add points to user score and render a "correct" to screen
+            //console.log("true")
+            return true; //change card
+          } else {
+            //console.log("false")
+            return false;
+          }
+        }
+      }
     },
-    showNotVideo() {
-      document.getElementById("notVideo").style.display = "inline-block";
-    },
+    sortScoreboard(scores) {
+      for (let i = 0; i < scores.length; i++) {
+        for (let j = 0; j < scores.length - 1; j++) {
+          if (scores[j].points < scores[j + 1].points) {
+              let tmp = scores[j];
+              scores[j] = scores[j + 1];
+              scores[j + 1] = tmp;
+          }
+        }
+      }
+      console.log(scores);
+      return scores;
+    }
   },
 };
 </script>
+<style src="../assets/css/Play.css">
+</style>
